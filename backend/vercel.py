34030583cd34a4e25df32bb7fecfaf7f27dc5aba
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-02-18 14:52:30 -0600

    fixing backend static build

--- backend/vercel.py
+++ backend/vercel.py
@@ -1,1 +1,5 @@
+import os
 from washbuddy.wsgi import application as app
+from whitenoise import WhiteNoise
+
+app = WhiteNoise(app, root=os.path.join(os.path.dirname(__file__), 'static'))
--- backend/washbuddy/settings.py
+++ backend/washbuddy/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = []
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
