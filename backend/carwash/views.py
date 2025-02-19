949be293bc171e18ca93bdd23bd5d15c5ecb6080
noErrdev <alexei.denis0328@gmail.com>
2025-02-18 19:41:12 -0700

    edit carwash CRUD

--- backend/carwash/views.py
+++ backend/carwash/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from .models import CarWash, CarWashOperatingHours, CarWashImage
 from .serializers import CarWashSerializer, CarWashOperatingHoursSerializer, CarWashImageSerializer
 
 class CarWashViewSet(viewsets.ModelViewSet):
     queryset = CarWash.objects.all()
     serializer_class = CarWashSerializer
+
+    def get_queryset(self):
+        return CarWash.objects.prefetch_related('carwashoperatinghours_set', 'carwashimage_set')
 
 class CarWashOperatingHoursViewSet(viewsets.ModelViewSet):
     queryset = CarWashOperatingHours.objects.all()
