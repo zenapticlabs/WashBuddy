6aa6c3b84dddd007060f1ba75e03e6c8fb58089d
noErrdev <alexei.denis0328@gmail.com>
2025-02-18 19:46:52 -0700

    reset carwash CRUD

--- backend/carwash/serializers.py
+++ backend/carwash/serializers.py
@@ -2,58 +2,17 @@
 from rest_framework import serializers
 from .models import CarWash, CarWashOperatingHours, CarWashImage
 
-class CarWashOperatingHoursSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CarWashOperatingHours
-        exclude = ('car_wash',)
-
-class CarWashImageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CarWashImage
-        exclude = ('car_wash',)
-
 class CarWashSerializer(serializers.ModelSerializer):
-    operating_hours = CarWashOperatingHoursSerializer(many=True)
-    images = CarWashImageSerializer(many=True)
-
     class Meta:
         model = CarWash
         fields = '__all__'
 
-    def create(self, validated_data):
-        operating_hours_data = validated_data.pop('operating_hours')
-        images_data = validated_data.pop('images')
+class CarWashOperatingHoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarWashOperatingHours
+        fields = '__all__'
 
-        # Create CarWash instance
-        car_wash = CarWash.objects.create(**validated_data)
-
-        # Create Operating Hours
-        for hours_data in operating_hours_data:
-            CarWashOperatingHours.objects.create(car_wash=car_wash, **hours_data)
-
-        # Create Images
-        for image_data in images_data:
-            CarWashImage.objects.create(car_wash=car_wash, **image_data)
-
-        return car_wash
-
-    def update(self, instance, validated_data):
-        operating_hours_data = validated_data.pop('operating_hours', [])
-        images_data = validated_data.pop('images', [])
-
-        # Update CarWash fields
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        instance.save()
-
-        # Update Operating Hours
-        instance.carwashoperatinghours_set.all().delete()
-        for hours_data in operating_hours_data:
-            CarWashOperatingHours.objects.create(car_wash=instance, **hours_data)
-
-        # Update Images
-        instance.carwashimage_set.all().delete()
-        for image_data in images_data:
-            CarWashImage.objects.create(car_wash=instance, **image_data)
-
-        return instance
+class CarWashImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CarWashImage
+        fields = '__all__'
