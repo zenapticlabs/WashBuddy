8371f6663702e6b84717d7e6853f8a446b2949c4
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-02-21 17:14:02 -0600

    Fixed vercel project pipeline

--- backend/admin_dashboard/management/commands/create_admin.py
+++ backend/admin_dashboard/management/commands/create_admin.py
@@ -17,6 +17,6 @@ class Command(BaseCommand):
 
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f"✅ Superuser '{username}' created successfully."))
+            self.stdout.write(self.style.SUCCESS(f"✅ Superuser '{username}' created successfully!"))
         else:
             self.stdout.write(self.style.WARNING(f"⚠️ Superuser '{username}' already exists."))
