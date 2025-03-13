c6502f36e17674051e644ce58dfa4caf4b75ceaa
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-03-12 20:33:42 -0500

    Changed id fields to uuid

--- backend/carwash/urls.py
+++ backend/carwash/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, include
 from .views import ListCarWashAPIView, CarWashViewSet, AmenityViewSet, WashTypeViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
-router.register(r'carwashes', CarWashViewSet)
-router.register(r'amenities', AmenityViewSet)
-router.register(r'wash-types', WashTypeViewSet)
+# router = DefaultRouter()
+# router.register(r'carwashes', CarWashViewSet)
+# router.register(r'amenities', AmenityViewSet)
+# router.register(r'wash-types', WashTypeViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    # path('', include(router.urls)),
     path("carwash/search/", ListCarWashAPIView.as_view(), name="list-car-wash"),
 ]
