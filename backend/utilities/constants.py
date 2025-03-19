91d86f7666f9414ba56a3cb570a102738380b040
Hamza Zaid <hamza.zaid29@yahoo.com>
2025-03-19 16:09:21 +0500

    chore: abstract model added for status

---
+++ backend/utilities/constants.py
@@ -0,0 +1,14 @@
+DEFAULT_STATUS_CHOICES = (
+    ("ACTIVE", "Active"),
+    ("INACTIVE", "Inactive"),
+    ("PENDING", "Pending"),
+    ("EXPIRED", "Expired"),
+    ("NOT_STARTED", "Not Started"),
+    ("CANCELLED", "Cancelled"),
+    ("APPROVAL", "Approval"),
+    ("REQUEST", "Request"),
+    ("REVIEW", "Review"),
+    ("VALIDATION", "Validation"),
+    ("CLOSURE", "Closure"),
+    ("COMPLETED", "Completed")
+)
