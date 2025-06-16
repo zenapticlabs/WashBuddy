4fff384185b3d0830e4d7930001c3df3e40f296f
Hamza Zaid <hamza.zaid29@yahoo.com>
2025-06-16 17:50:58 +0500

    chore: email integrated

--- backend/washbuddy/settings.py
+++ backend/washbuddy/settings.py
@@ -208,3 +208,12 @@ SPECTACULAR_SETTINGS = {
         }
     }
 }
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = 'WashBuddy Team <support@washbuddy.com>'
