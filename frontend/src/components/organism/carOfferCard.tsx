09bf86ad1c90f0d1179aa9219cf09f38e5c809e3
loi-go-1220 <loi.go1220@gmail.com>
2025-07-13 12:58:29 -0500

    Frontend (#119)
    * Fix small issues on Review
    
    * Fix: Sort Carwash Package Price
    
    * Fix on Amenities Select
    
    * Get the WashTypes and Amenities from Backend
    
    * Consider the null of the Category in Amenities
    
    * Fix issues on Washtypes
    
    * Update fetching wash types
    
    * Fix small issues on Timezone and Hidden Offer distance
    
    * Fix small issue
    
    * c
    
    * Fix small issues
    
    * Fix small issues
    
    * Update Auth Flow, Bounty Page Update
    
    * Update Workflow(Bounty, Auth Workflow, small issue)
    
    * feat: offer filter checkbox, Bounty highlight, fix issue on successfull pay page
    
    * feat: Add Support Email Button to Hamburger Menu
    
    * Feat: Add Redemption Page
    
    * Update Deal Text for Hidden Carwash Offers and Fix error on Radar Map
    
    * Fix build err in redemption page
    
    * Fix small issues
    
    * Feat: Offer Filter
    
    * Fix Login issue
    
    * Small change in claim error message
    
    * fix error
    
    * Fix: Search Z-Index Error
    
    * Update Offer Bounty Image, Hidden Offer Layout
    
    * Fix issues

--- frontend/src/components/organism/carOfferCard.tsx
+++ frontend/src/components/organism/carOfferCard.tsx
@@ -1,5 +1,7 @@
-import { MapPinIcon } from "lucide-react";
+import { MapPinIcon, StarIcon, SparklesIcon } from "lucide-react";
 import { Badge } from "../ui/badge";
+import Image from "next/image";
+import hiddenOfferImage from "@/assets/bountyimg-bg-removal.png";
 
 interface CarOfferCardProps {
   onClick: () => void;
@@ -8,53 +10,92 @@ interface CarOfferCardProps {
 }
 
 export function CarOfferCard({ onClick, isSelected, data }: CarOfferCardProps) {
-  const handleClick = () => {
-    onClick();
-  }
   return (
     <>
       <div
-        className={`p-3 border rounded-lg cursor-pointer transition-all duration-300 ${isSelected
-          ? "border-blue-500 bg-blue-50"
-          : "border-neutral-50 hover:border-blue-500"
-          }`}
-        onClick={handleClick}
+        className={`p-3 border rounded-lg cursor-pointer transition-all duration-300 ${
+          isSelected
+            ? "border-blue-500 bg-blue-50"
+            : "border-yellow-400 border-2 hover:border-yellow-400"
+        }`}
+        onClick={onClick}
       >
         <div className="flex gap-2 rounded-lg w-full">
-          <div
-            className="relative cursor-pointer group"
-          >
-            <div className="w-16 h-16 md:w-24 md:h-24 object-cover rounded transition-transform group-hover:scale-105 text-red-500 md:text-8xl text-5xl font-bold bg-neutral-50 flex items-center justify-center">?</div>
+          <div className="relative cursor-pointer group">
+            <div className="w-16 h-16 md:w-24 md:h-24 relative rounded overflow-hidden">
+              <Image 
+                src={hiddenOfferImage} 
+                alt="car-offer-card" 
+                className="object-cover transition-transform group-hover:scale-105"
+                fill
+                priority={true}
+                loading="eager"
+                quality={75}
+              />
+            </div>
             <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-20 rounded transition-opacity" />
           </div>
           <div className="flex flex-col justify-between flex-1">
             <div>
-              <div className="text-title-2 md:text-title-1 text-neutral-900">
-                Secret Wash Deal Near You: {data?.offer_price ? `: $${data?.offer_price}` : ""}
+              <div className="flex items-center gap-2">
+                <div className="text-title-2 md:text-title-1 text-neutral-900">
+                  Secret Wash Deal Near You
+                </div>
+                <SparklesIcon className="w-5 h-5 text-yellow-500" />
               </div>
               <div className="text-body-3 md:text-body-2 flex items-center text-neutral-500 mt-1">
                 <MapPinIcon className="w-4 h-4 md:w-5 md:h-5 pr-1" />
-                <div>Unlock the deal
+                <div>
+                  Unlock the deal
                   <span className="px-1">·</span>
                   Just minutes away
                 </div>
               </div>
+              <Badge
+                variant="blue"
+                className="text-title-3 text-white w-fit hidden md:block px-2 py-1 rounded-lg mt-2"
+              >
+                Mystery Wash · WashBuddy Exclusive
+              </Badge>
             </div>
-            <Badge
-              variant="blue"
-              className="text-title-3 text-white w-fit hidden md:block px-2 py-1 rounded-lg mt-2"
-            >
-              Mystery Wash
-              <span className="px-1">·</span>
-              WashBuddy Exclusive
-            </Badge>
+            <div className="md:hidden flex items-center justify-between">
+              <div className="flex items-center gap-0.5">
+                <StarIcon className="w-3.5 h-3.5 text-accent-yellow fill-accent-yellow" />
+                <span className="text-title-2 text-accent-yellow">
+                  {data?.rating || "?"}<span className="text-body-3 text-neutral-300 pl-1">(Secret)</span>
+                </span>
+              </div>
+              <div className="text-headline-4 py-1 text-neutral-900 text-right pl-4">
+                ${data?.offer_price || "3"}
+              </div>
+              <div className="flex-1 text-body-3 text-neutral-900 text-right">
+                Special Offer
+              </div>
+            </div>
+          </div>
+          <div className="hidden md:flex flex-col justify-end border-l border-neutral-50 pl-4">
+            <div className="flex items-center gap-0.5 justify-end">
+              <StarIcon className="w-3.5 h-3.5 text-accent-yellow fill-accent-yellow" />
+              <span className="text-title-2 text-accent-yellow">
+                {data?.rating || "?"}
+              </span>
+              <span className="text-body-3 text-neutral-300">
+                (Secret)
+              </span>
+            </div>
+            <div className="text-headline-4 py-1 text-neutral-900 text-right">
+              ${data?.offer_price || "3"}
+            </div>
+            <div className="flex-1 text-body-3 text-neutral-900 text-right">
+              Special Offer
+            </div>
           </div>
         </div>
         <Badge
           variant="blue"
-          className="text-title-3 text-white w-fit block md:hidden px-2 py-1 rounded-lg mt-2"
+          className="text-title-3 text-white w-fit md:hidden px-2 py-1 rounded-lg mt-2"
         >
-          Nearby Special Washbuddy Price.
+          Mystery Wash · WashBuddy Exclusive
         </Badge>
       </div>
     </>
