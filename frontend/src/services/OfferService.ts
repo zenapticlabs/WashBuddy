310c8c66e6f7678a6c1dadef3e8604a15627c36a
loi-go-1220 <loi.go1220@gmail.com>
2025-07-18 14:59:09 -0500

    Small issues (#125)
    * Update the landing page
    
    * Update small issues on Landing
    
    * Update small issues on Carwash offer, claim
    
    * Fix small issue

--- frontend/src/services/OfferService.ts
+++ frontend/src/services/OfferService.ts
@@ -1,11 +1,14 @@
 import axiosInstance from '../lib/axios';
 
-export const getOffers = async (userLat: number, userLng: number) => {
+export const getOffers = async (filters: Record<string, any> = {}) => {
+    const params = new URLSearchParams();
+    Object.entries(filters).forEach(([key, value]) => {
+        if (value) {
+            params.append(key, value.toString());
+        }
+    });
     const response = await axiosInstance.get('/api/v1/carwash/offers/search', {
-        params: {
-            userLat,
-            userLng,
-        },
+        params,
     });
     return response.data;
 };
